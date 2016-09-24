5a953ae6999dc6e47169910bb892b4a44fdafeaf
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-09-24 14:45:24 -0400

    intermediate revision - refactoring

--- getPagesSource.js
+++ getPagesSource.js
@@ -1,61 +1,60 @@
 function DOMtoString(document_root) {
-    // cloud-init.log
-    var html = '',
-
-
-        node = document_root.firstChild;
-    while (node) {
-        switch (node.nodeType) {
-            case Node.ELEMENT_NODE:
-
-                // 	html += node.outerHTML;
-                var data = node.innerHTML;
-                var log_lines = data.split(/\n/);
-                var matching_lines = '';
-
-                var count = log_lines.length;
-                // count = 3000;
-                for (i = 0; i < count; i++) {
-                    log_line = log_lines[i];
-                    var date_header = new RegExp('[\\d-]+\\s+[\\d:]+\\s+');
-                    var pid_header = new RegExp('\\[pid:\\d+\\]\\s+');
-                    var provisioner_header = new RegExp('(?:cloud-provisioner.subprocess|) INFO:');
-                    var misc_header1 = new RegExp('(Notice:|returns:)','g');
-                    var misc_header2 = new RegExp('\\[\\d?m','g');
-                    var patt = new RegExp('/Stage.*Exec\\[reporter\\]');
-                    if (patt.test(log_line)) {
-                        xx = log_line.replace(date_header, '').replace(pid_header, '').replace(provisioner_header, '').replace(misc_header1, '').replace(misc_header2, '');
-                        matching_lines += xx.replace(patt, '') + '\n';
-                        // matching_lines += log_line.replace(patt, '') + '\n';
-                        //  matching_lines += log_lines[i] + '\n';
-                    }
-                }
-                var length = matching_lines.length;
-
-                html += matching_lines;
-                // html += length + ' lines';
-                // html += node.innerHTML;
-                break;
-            case Node.TEXT_NODE:
-                html += node.nodeValue;
-                break;
-            case Node.CDATA_SECTION_NODE:
-                html += '<![CDATA[' + node.nodeValue + ']]>';
-                break;
-            case Node.COMMENT_NODE:
-                html += '<!--' + node.nodeValue + '-->';
-                break;
-            case Node.DOCUMENT_TYPE_NODE:
-                // (X)HTML documents are identified by public identifiers
-                html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-                break;
-        }
-        node = node.nextSibling;
-    }
-    return html;
+	var html = '';
+	var node = document_root.firstChild;
+	while (node) {
+		switch (node.nodeType) {
+			case Node.ELEMENT_NODE:
+				var process = false;
+				if (process) {
+					var data = node.innerHTML;
+					var log_lines = data.split(/\n/);
+					var matching_lines = '';
+					var count = log_lines.length;
+					for (i = 0; i < count; i++) {
+						log_line = log_lines[i];
+						var resource_pattern = new RegExp('/Stage.*Exec\\[reporter\\]');
+						if (resource_pattern.test(log_line)) {
+							var matching_line = log_line.replace(resource_pattern, '');
+							for (var remove_part in [
+									'[\\d-]+\\s+[\\d:]+\\s+', // timestamp
+									'\\[pid:\\d+\\]\\s+', // pid
+									'(?:cloud-provisioner.subprocess|) INFO:', // wrapper 
+									'(Notice:|returns:)', // message category
+									'\\[\\d?m' // text attribute character 
+								]) {
+								matching_line = matching_line.replace(new RegExp(remove_part, 'g'), '');
+							}
+							matching_lines += matching_line + '\n';
+							// matching_lines += log_line.replace(patt, '').replace(date_header, '').replace(pid_header, '').replace(provisioner_header, '').replace(misc_header1, '').replace(misc_header2, '') + '\n';
+						}
+					}
+					html += matching_lines;
+					// Summary 
+					html += matching_lines.length + ' lines';
+				} else {
+					html += node.outerHTML;
+				}
+				break;
+			case Node.TEXT_NODE:
+				html += node.nodeValue;
+				break;
+			case Node.CDATA_SECTION_NODE:
+				html += '<![CDATA[' + node.nodeValue + ']]>';
+				break;
+			case Node.COMMENT_NODE:
+				html += '<!--' + node.nodeValue + '-->';
+				break;
+			case Node.DOCUMENT_TYPE_NODE:
+				// (X)HTML documents are identified by public identifiers
+				html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+				break;
+		}
+		node = node.nextSibling;
+	}
+	return html;
 }
 
 chrome.runtime.sendMessage({
-    action: "getSource",
-    source: DOMtoString(document)
+	action: "getSource",
+	source: DOMtoString(document)
 });
--- popup.js
+++ popup.js
@@ -13,7 +13,7 @@ function onWindowLoad() {
   }, function() {
     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
     if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+      message.innerText = 'Error injecting script : \n' + chrome.runtime.lastError.message;
     }
   });
 
