3f93b79122b0ea1a95ad4bc29294b8956567f8e4
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-09-28 22:12:53 -0400

    intermediate revision -  refactoring

--- getPagesSource.js
+++ getPagesSource.js
@@ -1,47 +1,61 @@
 function DOMtoString(document_root) {
 	var html = '';
-	var cnt = 0;
 	var node = document_root.firstChild;
-	while (node && cnt == 0) {
+	while (node) {
 		switch (node.nodeType) {
 			case Node.ELEMENT_NODE:
-				var process = true;
-				if (process) {
-					// alert('filter: ' + config.filter+ '\n' + 'reporter: ' + config.reporter + '\n' + 'cnt: ' + cnt );
-					var data = node.innerHTML;
-					var log_lines = data.split(/\n/);
-					var matching_lines = '';
-					var count = log_lines.length;
-					for (i = 0; i < count; i++) {
-						log_line = log_lines[i];
-						if (config.filter == 'spec') {
-							var resource_pattern = new RegExp('/Stage.*Exec\\[reporter\\]/');
-							if (resource_pattern.test(log_line)) {
-								var matching_line = log_line.replace(resource_pattern, '');
-
-								var remove_patterns = ['\\d{2,2}\\-\\d{2,2}\\-\\d{2,2}\\s+[\\d:]+\\s+',
-									'\\[pid:\\d+\\]\\s+',
-									'(?:cloud-provisioner.subprocess|cloud-provisioner)',
-									'\\s+INFO:\\s+',
-									'(?:Notice|returns):',
-									'\\[\\d?m'
-								];
-								for (var cnt = 0; cnt < remove_patterns.length; cnt++) {
-									var p = new RegExp(remove_patterns[cnt], 'g')
-									matching_line = matching_line.replace(p, '');
-								}
-								matching_lines += matching_line + '\n';
+				var data = node.innerHTML;
+				var log_lines = data.split(/\n/);
+				var matching_lines = [];
+				var count = log_lines.length;
+				// alert('config.filter :' + config.filter ) ;
+				for (line_cnt = 0; line_cnt < log_lines.length; line_cnt++) {
+					log_line = log_lines[line_cnt];
+					if (config.filter == 'errors') {
+						var resource_pattern = new RegExp('(?:failed|failures)');
+						if (resource_pattern.test(log_line)) {
+							var main_header = new RegExp('/Stage\\[.*/([^/.]+)/([^/.]+):\\s+');
+							log_line = log_line.replace(main_header, '$1/$2: ');
+							var dependency_header = new RegExp('/Stage\\[.*/([^/.]+):\\s+Dependency');
+							log_line = log_line.replace(dependency_header, '$1: Dependency');
+							var header = new RegExp('/Stage\\[.*/([^/.]+):\\s+Dependency');
+							log_line = log_line.replace(header, '$1: Dependency');
+							var remove_patterns = ['\\d{2,2}\\-\\d{2,2}\\-\\d{2,2}\\s+[\\d:]+\\s+',
+								// '/Stage\\[.*\\]:\\s+Dependency',
+								'\\[pid:\\d+\\]\\s+',
+								'(?:cloud-provisioner.subprocess|cloud-provisioner)',
+								'\\s+INFO:\\s+',
+								'(?:Notice|returns):',
+								'\\[\\d?m',
+								'.\\[1;31m',
+							];
+							for (var cnt = 0; cnt < remove_patterns.length; cnt++) {
+								log_line = log_line.replace(new RegExp(remove_patterns[cnt], 'g'), '');
 							}
+							matching_lines.push(log_line);
 						}
 					}
-					html += matching_lines;
-					// Summary 
-					html += matching_lines.length + ' lines';
-					cnt++;
-
-				} else {
-					html += node.outerHTML;
+					if (config.filter == 'spec') {
+						var resource_pattern = new RegExp('/Stage.*Exec\\[reporter\\]/');
+						if (resource_pattern.test(log_line)) {
+							var matching_line = log_line.replace(resource_pattern, '');
+							var remove_patterns = ['\\d{2,2}\\-\\d{2,2}\\-\\d{2,2}\\s+[\\d:]+\\s+',
+								'\\[pid:\\d+\\]\\s+',
+								'(?:cloud-provisioner.subprocess|cloud-provisioner)',
+								'\\s+INFO:\\s+',
+								'(?:Notice|returns):',
+								'\\[\\d?m'
+							];
+							for (var cnt = 0; cnt < remove_patterns.length; cnt++) {
+								matching_line = matching_line.replace(new RegExp(remove_patterns[cnt], 'g'), '');
+							}
+							matching_lines.push(matching_line);
+						}
+					}
 				}
+				html += matching_lines.join('\n');
+				// Summary 
+				html += '\n' + matching_lines.length + ' lines';
 				break;
 			case Node.TEXT_NODE:
 				html += node.nodeValue;
